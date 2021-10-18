da9368d347738a45c75ce242b492ce4f1ff7960d
Rbster <rbster@yandex.ru>
2021-10-18 17:18:31 +0300

    webserver: Ok

--- webserver/src/main/kotlin/FileServer.kt
+++ webserver/src/main/kotlin/FileServer.kt
@@ -29,85 +29,34 @@ class FileServer {
          * ServerSocket object.
          */
         while (true) {
+            val clientSocket = socket.accept()
+            clientSocket.use { s ->
+                // read request
+                val reader = s.getInputStream().bufferedReader()
+                val clientRequest = reader.readLine()
+                // process request
+                val requestParts = clientRequest?.split(' ')
+                val vPath: VPath?
+                var content: String? = null
+                if (requestParts != null &&
+                    requestParts.size == 3 &&
+                    (requestParts[2] == "HTTP/1.1" ||
+                            requestParts[2] == "HTTP/1.0" ||
+                            requestParts[2] == "HTTP/2") &&
+                    requestParts[0] == "GET"
+                ) {
 
-            // TODO Delete this once you start working on your solution.
-            //throw new UnsupportedOperationException();
-
-            // TODO 1) Use socket.accept to get a Socket object
-            var clientSocket = socket.accept()
-
-            /*
-            * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-            * packet. In particular, we are interested in confirming this
-            * message is a GET and parsing out the path to the file we are
-            * GETing. Recall that for GET HTTP packets, the first line of the
-            * received packet will look something like:
-            *
-            *     GET /path/to/file HTTP/1.1
-            */
-            var requestString: String?
-            var requestParts: List<String>?
-            var vPath: VPath?
-            var content: String? = null
-            var clientInputStream = clientSocket.getInputStream()
-
-            clientInputStream.use {
-                it.bufferedReader().use {
-                    requestString = it.readLine()
+                    vPath = getVPathOrNull(requestParts[1])
+                    content = if (vPath != null) fs.readFile(vPath) else null
                 }
+
+                // send response
+                val writer = s.getOutputStream().bufferedWriter()
+                writer.write(responseString(content))
+                writer.flush()
             }
 
-
-
-            requestParts = requestString?.split(' ')
-            if (requestParts != null &&
-                requestParts.size == 3 &&
-                (requestParts[2] == "HTTP/1.1" ||
-                requestParts[2] == "HTTP/1.0" ||
-                requestParts[2] == "HTTP/2") &&
-                requestParts[0] == "GET") {
-
-                vPath = getVPathOrNull(requestParts[1])
-                content = if (vPath != null) fs.readFile(vPath) else null
-
-            }
-
-
-
-            var clientOutputStream = clientSocket.getOutputStream()
-            clientOutputStream.use {
-                it.bufferedWriter().use {
-                    it.write(responseString(content))
-                }
-            }
-
-
-            clientSocket.close()
-
-
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
         }
-
-
 
     }
 
