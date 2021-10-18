ff452c68c8b9c10938ecd24bd9735aa75b95e4c0
Rbster <rbster@yandex.ru>
2021-10-19 01:34:49 +0300

    spring-bean-lifecycle: true KO

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -4,55 +4,25 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
-import kotlin.reflect.KClass
+import org.springframework.util.ClassUtils
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
-        beanFactory.getBeanDefinition("beanFactoryPostProcessorBean").initMethodName = "postConstruct"
-//        beanFactory.getDependentBeans("beanFactoryPostProcessorBean")
-//        println("here: ")
-//        for (beanDefinitionName in beanFactory.beanDefinitionNames) {
-//            if (beanDefinitionName == "beanFactoryPostProcessorInterface") {
-//                println("name: $beanDefinitionName")
-//
-//            }
-//            if (beanDefinitionName == "BeanFactoryPostProcessorInterface") {
-//                println(beanDefinitionName + "fields: ")
-//            }
-//            for (method in (beanFactory.getBean(beanDefinitionName).javaClass.declaredMethods)) {
-//
-//                if (beanDefinitionName == "BeanFactoryPostProcessorInterface") {
-//                    print(method.name + "; ")
-//                }
-//                if (method.isAnnotationPresent(PostConstruct::class.java)) {
-//                    for (postConstructDependantBeanName in beanFactory.getDependentBeans(beanDefinitionName)) {
-//                        beanFactory
-//                            .getBeanDefinition(postConstructDependantBeanName)
-//                            .initMethodName = method.name
-//                    }
-//                }
-//            }
-
-
-//            if (beanDefinitionName == "beanFactoryPostProcessorBean") {
-//                println(beanDefinitionName + " depend on:")
-//            }
-//            for (dependentOnBeanName in beanFactory.getDependenciesForBean(beanDefinitionName)) {
-//                println("""$beanDefinitionName depend on $dependentOnBeanName""")
-//            }
-
-//            for (method in (beanFactory.getBean(beanDefinitionName).javaClass.declaredMethods)) {
-//                if (beanDefinitionName == "beanFactoryPostProcessorBean") println(method.name + " " + method.annotations.size)
-//                if (method.isAnnotationPresent(PostConstruct::class.java)) {
-//                    println("bean name: $beanDefinitionName\n" +
-//                            "method name: ${method.name}")
-//                    beanFactory
-//                        .getBeanDefinition(beanDefinitionName)
-//                        .initMethodName = method.name
-//                }
-//            }
-//    }
-
+        var className: String?
+        for (bName in beanFactory.beanDefinitionNames) {
+            className = beanFactory.getBeanDefinition(bName).beanClassName
+            if (className != null) {
+                for (intFace in ClassUtils
+                    .getAllInterfacesForClass(ClassUtils.forName(className, beanFactory.beanClassLoader))
+                ) {
+                    for (method in intFace.declaredMethods) {
+                        if (method.isAnnotationPresent(PostConstruct::class.java)) {
+                            beanFactory.getBeanDefinition(bName).initMethodName = method.name
+                        }
+                    }
+                }
+            }
+        }
     }
 }
