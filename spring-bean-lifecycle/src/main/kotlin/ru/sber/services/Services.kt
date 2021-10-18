1962cf0fb652e5e5c5a33af90fc325c7eec1094d
Rbster <rbster@yandex.ru>
2021-10-18 21:56:43 +0300

    spring-bean-lifecycle: ok without (*)

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/Services.kt
@@ -2,6 +2,7 @@ package ru.sber.services
 
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
@@ -10,21 +11,24 @@ class CallbackBean : InitializingBean, DisposableBean {
     var greeting: String? = "What's happening?"
 
     override fun afterPropertiesSet() {
+        greeting = "Hello! My name is callbackBean!"
     }
 
     override fun destroy() {
         greeting = "Sorry, but I really have to go."
     }
+
+
 }
 
-class CombinedBean {
+class CombinedBean : InitializingBean {
     var postProcessBeforeInitializationOrderMessage: String? = null
     var postConstructOrderMessage: String? = null
     var customInitOrderMessage: String? = null
     var afterPropertiesSetOrderMessage: String? = null
     var postProcessAfterInitializationOrderMessage: String? = null
 
-    fun afterPropertiesSet() {
+    override fun afterPropertiesSet() {
         afterPropertiesSetOrderMessage = "afterPropertiesSet() is called"
     }
 
@@ -32,10 +36,37 @@ class CombinedBean {
         customInitOrderMessage = "customInit() is called"
     }
 
+    @PostConstruct
     fun postConstruct() {
         postConstructOrderMessage = "postConstruct() is called"
     }
+
+
 }
+
+@Component
+class InitialisePropertiesBeanPostProcessor : BeanPostProcessor {
+    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        if (beanName == "combinedBean") {
+            val b: CombinedBean = bean as CombinedBean
+            b.postProcessBeforeInitializationOrderMessage = "postProcessBeforeInitialization() is called"
+            return b
+        } else {
+            return bean
+        }
+    }
+    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        if (beanName == "combinedBean") {
+            val b: CombinedBean = bean as CombinedBean
+            b.postProcessAfterInitializationOrderMessage = "postProcessAfterInitialization() is called"
+            return b
+        } else {
+            return bean
+        }
+    }
+}
+
+
 
 @Component
 class BeanFactoryPostProcessorBean : BeanFactoryPostProcessorInterface {
