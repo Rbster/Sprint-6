4f862c1311a04a0897533e6d389951f936e91427
Rbster <rbster@yandex.ru>
2021-10-20 16:31:39 +0300

    spring-mvc: filters ok?

--- spring-mvc/src/main/kotlin/ru/sber/mvcExample/MvcExampleApplication.kt
+++ spring-mvc/src/main/kotlin/ru/sber/mvcExample/MvcExampleApplication.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import ru.sber.mvcExample.servlet.MainServlet
 
 @SpringBootApplication
+@EnableWebMvc
 class MvcExampleApplication {
 	@Bean
 	fun MainServletBean(): ServletRegistrationBean<*> {
--- spring-mvc/src/main/kotlin/ru/sber/mvcExample/filters/Filters.kt
+++ spring-mvc/src/main/kotlin/ru/sber/mvcExample/filters/Filters.kt
@@ -1,29 +1,64 @@
 package ru.sber.mvcExample.filters
 
-import org.springframework.context.annotation.ComponentScan
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
+import org.springframework.web.filter.CommonsRequestLoggingFilter
 import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
 
 @Component
 @Order(1)
-class LogFilter : Filter {
-    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        println("-----> Filtering in LogFilter")
-        chain!!.doFilter(request, response)
+class LoggingFilter : Filter {
+    companion object {
+        var logger: Log = LogFactory.getLog(CommonsRequestLoggingFilter::class.java)
     }
 
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        println("-----> Filtering in CheckAuthFilter")
+        val httpRequest = request as HttpServletRequest
+        logger.info(httpRequest.method + " request from " + httpRequest.remoteAddr)
+        chain!!.doFilter(request, response)
+        println("<----- out CheckAuthFilter")
+    }
 }
 
 @Component
 @Order(2)
-class CheckAuthFilter : Filter {
+class AuthFilter : Filter {
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        println("-----> Filtering in CheckAuthFilter")
-        chain!!.doFilter(request, response)
+        println("-----> Filtering in LogFilter")
+        val httpRequest = request as HttpServletRequest
+        val httpResponse = response as HttpServletResponse
+        var cookie: Cookie? = null
+        val loginURI = request.contextPath + "/login"
+        val requestURI = request.requestURI
+
+        if (httpRequest.cookies != null) {
+            for (q in httpRequest.cookies) {
+                if (q.name == "authorised") {
+                    cookie = q
+                }
+            }
+        }
+
+        if (requestURI != loginURI && (cookie == null || cookie.name != "something")) {
+            // redirect
+            println("<----- redirect from LogFilter")
+            httpResponse.sendRedirect(loginURI)
+
+        } else {
+            chain!!.doFilter(request, response)
+        }
+        println("<----- out LogFilter")
     }
 
 }
+
